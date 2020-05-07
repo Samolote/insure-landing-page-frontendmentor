9bfac93cb8f75d6564263305058e74163ba4b19f
Wojciech Łukowski <wojtek304@gmail.com>
2020-05-07 22:59:49 +0200

    Add overflow-y toggle for body element upon opening of burger menu

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const burger = document.querySelector('.burger');
+const body = document.querySelector('body');
 
 const burgerHandler = () => {
 	burger.classList.toggle('burger--active');
+	if (!body.style.overflowY && burger.classList.contains('burger--active')) {
+		body.style.overflowY = 'hidden';
+	} else {
+		body.style.overflowY = '';
+	}
 };
 
 burger.addEventListener('click', burgerHandler);
